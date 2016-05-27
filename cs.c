251991af377edc604ba0a15680c9471d981dd12a
mathioud <mathioudcsd@gmail.com>
2016-05-27 15:54:07 +0300

    the first upload for the c ClientServer example

---
+++ cs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include "practical.h"
+
+
+int main(int argc, char* argv[]){
+	
+}
